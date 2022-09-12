baff78e6d40c3a74d1a400c89d3aa14edb41e17a
wade <wade841208@gmail.com>
2022-09-12 15:04:35 +0800

    wade t1 T,W

--- src/app/TestApp.java
+++ src/app/TestApp.java
@@ -3,5 +3,6 @@ package app;
 public class TestApp {
 	public static void main(String[] args) {
 		System.out.println("hi");
+		System.out.println("hit1");
 	}
 }
--- src/app/wade.java
+++ src/app/wade.java
@@ -3,5 +3,6 @@ package app;
 public class wade {
 public static void main(String[] args) {
 	System.out.println("wade");
+	System.out.println("wadet1");
 }
 }
